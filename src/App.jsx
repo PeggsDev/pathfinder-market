9001338e155398fa4b1b00f040f5930c8940a94b
Pedro Francis <pedro.francis@aexp.com>
2022-12-21 12:06:30 +0000

    Added the initial setup for parallax scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -6,44 +6,86 @@ import PrimerBook from './img/pathfinder-primer.jpg'
 import Bestiary from './img/bestiary.jpg'
 import GameMasteryGuide from './img/game-mastery.jpg'
 
+import background from './img/parallax-background.jpg'
+
 function App() {
 
     return (
         <div>
             <NavBar/>
-            <div className={'card-block'}>
-                <CardComponent
-                    price={'£34.99'}
-                    bookName={'Core Rulebook'}
-                    imagePath={CoreRulebook}
-                    summary={
-                        `This indispensable volume contains the core rules for players and Game Masters, 
+            <div className={'parallax-wrapper'}>
+                <section className={'header'}>
+                    <img src={background} alt="background" className={'background'}/>
+                    {/*<section className={'card-block'}>*/}
+                    {/*    <CardComponent*/}
+                    {/*        price={'£34.99'}*/}
+                    {/*        bookName={'Core Rulebook'}*/}
+                    {/*        imagePath={CoreRulebook}*/}
+                    {/*        summary={*/}
+                    {/*            `This indispensable volume contains the core rules for players and Game Masters, */}
+                    {/*    and is your first step on a heroic new journey!`*/}
+                    {/*        }/>*/}
+                    {/*    <CardComponent*/}
+                    {/*        price={'£34.99'}*/}
+                    {/*        bookName={'Bestiary'}*/}
+                    {/*        imagePath={Bestiary}*/}
+                    {/*        summary={*/}
+                    {/*            `Over 400 of fantasy's fiercest foes burst from the pages of this enormous compendium */}
+                    {/*    of the most popular and commonly encountered creatures in the world of Pathfinder!`*/}
+                    {/*        }/>*/}
+                    {/*    <CardComponent*/}
+                    {/*        price={'£34.99'}*/}
+                    {/*        bookName={'Gamemastery Guide'}*/}
+                    {/*        imagePath={GameMasteryGuide}*/}
+                    {/*        summary={*/}
+                    {/*            `Whether you are a new Game Master or experienced storyteller, */}
+                    {/*    you can always find new ways to hone your craft.`*/}
+                    {/*        }/>*/}
+                    {/*    <CardComponent*/}
+                    {/*        price={'£0'}*/}
+                    {/*        bookName={'Pathfinder Primer'}*/}
+                    {/*        imagePath={PrimerBook}*/}
+                    {/*        summary={*/}
+                    {/*            `This free resource contains everything you need to learn how to play Pathfinder,*/}
+                    {/*including rules to create your own hero and tools to make your own amazing stories.`*/}
+                    {/*        }/>*/}
+                    {/*</section>*/}
+
+                </section>
+                <section className={'card-block'}>
+                    <CardComponent
+                        price={'£34.99'}
+                        bookName={'Core Rulebook'}
+                        imagePath={CoreRulebook}
+                        summary={
+                            `This indispensable volume contains the core rules for players and Game Masters, 
                         and is your first step on a heroic new journey!`
-                    }/>
-                <CardComponent
-                    price={'£34.99'}
-                    bookName={'Bestiary'}
-                    imagePath={Bestiary}
-                    summary={
-                        `Over 400 of fantasy's fiercest foes burst from the pages of this enormous compendium 
+                        }/>
+                    <CardComponent
+                        price={'£34.99'}
+                        bookName={'Bestiary'}
+                        imagePath={Bestiary}
+                        summary={
+                            `Over 400 of fantasy's fiercest foes burst from the pages of this enormous compendium 
                         of the most popular and commonly encountered creatures in the world of Pathfinder!`
-                    }/>
-                <CardComponent
-                    price={'£34.99'}
-                    bookName={'Gamemastery Guide'}
-                    imagePath={GameMasteryGuide}
-                    summary={
-                        `Whether you are a new Game Master or experienced storyteller, 
+                        }/>
+                    <CardComponent
+                        price={'£34.99'}
+                        bookName={'Gamemastery Guide'}
+                        imagePath={GameMasteryGuide}
+                        summary={
+                            `Whether you are a new Game Master or experienced storyteller, 
                         you can always find new ways to hone your craft.`
-                    }/>
-                <CardComponent
-                    price={'£0'}
-                    bookName={'Pathfinder Primer'}
-                    imagePath={PrimerBook}
-                    summary={
-                        `This free resource contains everything you need to learn how to play Pathfinder,
+                        }/>
+                    <CardComponent
+                        price={'£0'}
+                        bookName={'Pathfinder Primer'}
+                        imagePath={PrimerBook}
+                        summary={
+                            `This free resource contains everything you need to learn how to play Pathfinder,
                     including rules to create your own hero and tools to make your own amazing stories.`
-                    }/>
+                        }/>
+                </section>
             </div>
         </div>
     );
