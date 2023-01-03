0f84994d8e28d06148e4f05b5ba57bf805848620
Pedro Francis <pedro.francis@aexp.com>
2023-01-03 16:02:25 +0000

    WIP -  Added some svg fun times

--- src/pages/CharacterManagement/CharacterSheet.jsx
+++ src/pages/CharacterManagement/CharacterSheet.jsx
@@ -1,26 +1,77 @@
 import './CharacterSheet.scss'
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
+
+import { ReactComponent as Armor } from "./svg/armor.svg";
+
+let abilityScoreData = [
+    {
+        'id': '1',
+        'ability': 'strength',
+        'score': '14'
+    },
+    {
+        'id': '2',
+        'ability': 'dexterity',
+        'score': '20'
+    },
+    {
+        'id': '3',
+        'ability': 'Constitution',
+        'score': '8'
+    },
+    {
+        'id': '4',
+        'ability': 'Intelligence',
+        'score': '14'
+    },
+    {
+        'id': '5',
+        'ability': 'Wisdom',
+        'score': '16'
+    },
+    {
+        'id': '6',
+        'ability': 'Charisma',
+        'score': '18'
+    }
+]
 
 function calculateModifier(value) {
     let modifier = Math.floor((value - 10) / 2)
     return modifier > 0 ? "+" + modifier : modifier.toString();
 }
 
-function AbilityScore({ability}) {
-    const [score, setScore] = useState(10)
+function AbilityScore({ability, score}) {
     return (
         <div className={'ability'}>
-            <label className={'modifier'}>
-                <h3>{(ability.substring(0, 3)).toUpperCase()}</h3>
-                <h4>MODIFIER</h4>
-                <input type="text" value={calculateModifier(score)} onChange={() => {
-                }}/>
-            </label>
-            <label className={'score'}>
-                <h3>{ability.toUpperCase()}</h3>
-                <h4>SCORE</h4>
-                <input type="text" value={score} onChange={(event) => setScore(event.target.value)}/>
-            </label>
+            <h3>{ability.toUpperCase()}</h3>
+            <input className={'modifier'}
+                   type="text"
+                   disabled="disabled"
+                   placeholder="disabled"
+                   value={calculateModifier(score)}
+                   readOnly/>
+            <input className={'score'}
+                   type="text"
+                   disabled="disabled"
+                   placeholder="disabled"
+                   value={score}
+                   readOnly/>
+        </div>
+    )
+}
+
+function Skill({skill}) {
+    const [score, setScore] = useState(10)
+    return (
+        <div className={'skill'}>
+            <input className={'modifier'}
+                   type="text"
+                   disabled="disabled"
+                   placeholder="disabled"
+                   value={calculateModifier(score)}
+                   readOnly/>
+            <h3>{skill.toUpperCase()}</h3>
         </div>
     )
 }
@@ -28,15 +79,33 @@ function AbilityScore({ability}) {
 export default function CharacterSheet() {
     return (
         <div className={'character-sheet'}>
+            <div className={'armor-class'}>
+                <Armor className={'shield'} />
+                <h3>ARMOR</h3>
+                <input className={'modifier'}
+                       type="text"
+                       disabled="disabled"
+                       placeholder="disabled"
+                       value={10}
+                       readOnly/>
+                <h3>CLASS</h3>
+            </div>
             <div className={'ability-scores'}>
                 <h1>ABILITY SCORES</h1>
                 <form className={'score-block'}>
-                    <AbilityScore ability={'strength'}/>
-                    <AbilityScore ability={'dexterity'}/>
-                    <AbilityScore ability={'Constitution'}/>
-                    <AbilityScore ability={'Intelligence'}/>
-                    <AbilityScore ability={'Wisdom'}/>
-                    <AbilityScore ability={'Charisma'}/>
+                    {abilityScoreData.map((ability) => {
+                        return <AbilityScore key={ability.id}
+                                             ability={ability.ability}
+                                             score={ability.score}/>
+                    })}
+                </form>
+            </div>
+            <div className={'skill-modifiers'}>
+                <h1>SKILLS</h1>
+                <form className={'skills-block'}>
+                    <Skill skill={'Acrobatics'}/>
+                    <Skill skill={'Arcana'}/>
+                    <Skill skill={'Athletics'}/>
                 </form>
             </div>
         </div>
