9c4a9bf2eb1036f411cf2d1d29b356b21934a27a
Pedro Francis <peggsdev@gmail.com>
2023-01-06 10:09:22 +0000

    Added API fetch call for ancestry to the character builder

--- src/pages/CharacterManagement/CharacterBuilder.jsx
+++ src/pages/CharacterManagement/CharacterBuilder.jsx
@@ -1,8 +1,31 @@
 import './CharacterBuilder.scss'
+import React, { useEffect, useState } from "react";
 
 export default function CharacterBuilder() {
+    const [ancestries, setAncestry] = useState([])
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch('http://localhost:3001/character/ancestry');
+                const json = await response.json();
+                setAncestry(json)
+            } catch (error) {
+                console.log("error", error);
+            }
+        })();
+    }, []);
+
     return (
-        <>
-        </>
+        <div className='builder'>
+            <div className={'ancestry'}>
+                <select name={'ancestries'}> {
+                    ancestries.map((ancestry) => {
+                        return <option value={ancestry.id}>{ancestry.name}</option>
+                    })
+                }
+                </select>
+            </div>
+        </div>
     )
 }
