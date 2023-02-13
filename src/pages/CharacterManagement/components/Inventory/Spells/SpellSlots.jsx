a20e8edbddeddd248bfb9d044d062de00dd43204
Pedro Francis <peggsdev@gmail.com>
2023-02-13 16:03:14 +0000

    Mostly working just need to sort out some bugs with double clicking

--- src/pages/CharacterManagement/components/Inventory/Spells/SpellSlots.jsx
+++ src/pages/CharacterManagement/components/Inventory/Spells/SpellSlots.jsx
@@ -1,20 +1,20 @@
 import './SpellSlot.scss'
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-export default function SpellSlots({slotCount, spellLevel}) {
+export default function SpellSlots({ slotCount, spellLevel }) {
 
     const [spellSlots, setSpellSlots] = useState(Array(slotCount))
     const [burnSpell, setBurn] = useState('');
 
     useEffect(() => {
         setSpellSlots(spellSlots)
-    },[burnSpell])
+    }, [burnSpell])
 
     useEffect(() => {
         for (let index = 0; index < slotCount; index++) {
-             const key = spellLevel.toLowerCase().replace(/\s/g, '-') + '-slot-' + index
+            const key = spellLevel.toLowerCase().replace(/\s/g, '-') + '-slot-' + index
             spellSlots.splice(index, 1,
-                <div key={key} className={'spell-slot'} id={key} onClick={(e) => burnSpellSlot(e.target.id)}/>)
+                <div key={key} className={'spell-slot'} id={key} onClick={(e) => burnSpellSlot(e.target.id)} />)
         }
     }, [])
 
@@ -23,14 +23,13 @@ export default function SpellSlots({slotCount, spellLevel}) {
 
         const spellSlot = spellSlots[slotId.slice(-1)]
 
-        console.log(spellSlot.props.className.includes('burnt'))
-
-
         spellSlots.splice(slotId.slice(-1), 1,
             <div key={spellSlot.key}
-                 className={'spell-slot burnt'}
-                 id={spellSlot.props.id}
-                 onClick={(e) => burnSpellSlot(e.target.id)}/>)
+                className={`spell-slot ${spellSlot.props.className.includes('burnt') ? '' : 'burnt'}`}
+                id={spellSlot.props.id}
+                onClick={(e) => burnSpellSlot(e.target.id)} />)
+
+        setBurn(slotId)
     }
 
     return (
