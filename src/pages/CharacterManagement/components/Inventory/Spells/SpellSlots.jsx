ca2fb6d5c4de9aecaf60f35e7916a9719769eee0
Pedro Francis <pedro.francis@aexp.com>
2023-02-14 16:18:58 +0000

    WIP Spell slot PUT request

--- src/pages/CharacterManagement/components/Inventory/Spells/SpellSlots.jsx
+++ src/pages/CharacterManagement/components/Inventory/Spells/SpellSlots.jsx
@@ -3,13 +3,7 @@ import {useState} from "react";
 
 export default function SpellSlots({spells}) {
 
-    // const [spellSlotId, setSpellSlotId] = useState()
-    // const [spellSlot, setSpellSlot] = useState()
-    // const [spellSlotBurnStatus, setSpellSlotBurnStatus] = useState(false)
-    // const [requestUpdate, setRequestUpdate] = useState(false)
-
     const [spellSlotBurnStatus, setSpellSlotBurnStatus] = useState(false)
-
 
     function burnSlot(spellSlot, slotId) {
         console.log(slotId)
@@ -24,35 +18,6 @@ export default function SpellSlots({spells}) {
         window.location.reload()
         return response.json();
     }
-
-    // useEffect(() => {
-    //     if(requestUpdate) {
-    //         console.log(spellSlotId)
-    //         const requestOptions = {
-    //             method: 'PUT', // TODO - Fix this - Should be a POST but backend seems not to support it at the moment
-    //             headers: {'Content-Type': 'application/json'},
-    //             body: JSON.stringify(
-    //                 {"spellSlotId": spellSlotId, "burnt": spellSlotBurnStatus}
-    //             )
-    //         };
-    //         const response = fetch(`http://localhost:3001/character/spells/spell-slots/${spells.level}/${spellSlotId}`, requestOptions);
-    //         // window.location.reload()
-    //         return response.json();
-    //         setRequestUpdate(false)
-    //
-    //     }
-    //
-    // }, [spellSlotBurnStatus]);
-    //
-    //
-    // function updateBurnStatus(spellSlot, spellSlotId){
-    //     setRequestUpdate(true)
-    //     setSpellSlotId(spellSlotId)
-    //     setSpellSlot(spellSlot)
-    //     setSpellSlotBurnStatus(!spellSlotBurnStatus)
-    //
-    // }
-
 
     return (
         <div className={'spell-slots'}>
