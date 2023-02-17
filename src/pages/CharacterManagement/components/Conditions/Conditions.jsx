c31fdd87f64c94e6fcf5ce9481f399919848c6c9
Pedro Francis <pedro.francis@aexp.com>
2023-02-17 16:35:52 +0000

    Added WIP conditions component

--- src/pages/CharacterManagement/components/Conditions/Conditions.jsx
+++ src/pages/CharacterManagement/components/Conditions/Conditions.jsx
@@ -3,7 +3,11 @@ import './Conditions.scss'
 export default function Conditions(){
     return (
         <div className={'death-saves-wrapper'}>
-            Conditions
+            Army Conditions: Attitudes: Friendly, Helpful, Hostile, Indifferent, Unfriendly
+            Death and Dying: Doomed, Dying, Unconscious, Wounded
+            Degrees of Detection: Hidden, Observed, Undetected, Unnoticed
+            Lowered Abilities: Clumsy, Drained, Enfeebled, Stupefied
+            Senses: Blinded, Concealed, Dazzled, Deafened, Invisible
         </div>
     )
 }
