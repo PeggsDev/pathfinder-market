c927631e1d35904bf5c0ef2c0b738a7f886bc155
Pedro Francis <peggsdev@gmail.com>
2023-02-26 15:23:16 +0000

    updated account creation image to one i have hte rights to use

--- src/pages/AccountManagement/AccountRegistration.jsx
+++ src/pages/AccountManagement/AccountRegistration.jsx
@@ -1,12 +1,12 @@
 import './Account.scss'
-import Goblin from '../../images/Goblin_GoblinWarrior.png'
+import Goblin from '../../images/Goblin-Jugler.png'
 import ThreeDimensionalCard from "../../componenets/ThreeDimentionalCard/ThreeDimensionalCard";
 
 export default function AccountRegistration() {
     return (
         <div className={'account-registration-page'}>
             <ThreeDimensionalCard
-                title={'Creat an Account'}
+                title={'Create an Account'}
                 subTitle={'Signup with one of these popular providers'}
                 cardType={'Signup'}
                 image={Goblin}
